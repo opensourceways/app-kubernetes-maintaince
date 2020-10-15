7c6acc642a340846783aac3ee9cabcb59f5305e3
TommyLike <tommylikehu@gmail.com>
2020-10-15 19:27:52 +0800

    Remove basic http auth

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,12 +2,10 @@
 
 KUBECONFIG=""
 TTYD_PORT=${TTYD_PORT:-8080}
-TTYD_USERNAME=${TTYD_USERNAME:-"root"}
-TTYD_PASSWORD=${TTYD_PASSWORD:-"password"}
 START_CMD=${START_CMD:-"zsh"}
 for entry in `ls ~/.kube/*.yaml`
 do
   KUBECONFIG=${KUBECONFIG}${entry}:
 done
 export KUBECONFIG=${KUBECONFIG}
-ttyd --port ${TTYD_PORT} --credential ${TTYD_USERNAME}:${TTYD_PASSWORD} ${START_CMD}
+ttyd --port ${TTYD_PORT} ${START_CMD}
