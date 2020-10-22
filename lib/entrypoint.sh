aa535b5abaf2208a44be6101322b98bcb8877595
TommyLike <tommylikehu@gmail.com>
2020-10-22 09:12:12 +0800

    Add basic auth back

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,10 +2,18 @@
 
 KUBECONFIG=""
 TTYD_PORT=${TTYD_PORT:-8080}
+TTYD_USERNAME=${TTYD_USERNAME:-""}
+TTYD_PASSWORD=${TTYD_PASSWORD:-""}
 START_CMD=${START_CMD:-"zsh"}
 for entry in `ls ~/.kube/*.yaml`
 do
   KUBECONFIG=${KUBECONFIG}${entry}:
 done
 export KUBECONFIG=${KUBECONFIG}
-ttyd --port ${TTYD_PORT} ${START_CMD}
+
+if [ -z ${TTYD_USERNAME}  ]
+then
+  ttyd --port ${TTYD_PORT} ${START_CMD}
+else
+  ttyd --port ${TTYD_PORT} --credential ${TTYD_USERNAME}:${TTYD_PASSWORD} ${START_CMD}
+fi
